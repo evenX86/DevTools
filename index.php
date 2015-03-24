aba8c85a3f6e4d9c3de73c5c2aba9ee727059189
unknown <xuyifei01@sh-xuyifei01.SNDA.ROOT.CORP>
2015-03-24 14:20:14 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xuyifei01
+ * Date: 2015/3/24
+ * Time: 9:58
+ */
---
+++ js/tools.js
@@ -0,0 +1,47 @@
+/**
+ * Created by xuyifei01 on 2015/3/24.
+ */
+var currentTimeActive = 1;
+var unixTimer = 0;
+function unix2human() {
+    var dateObj = new Date(document.unix2beijing.timestamp.value * 1000);
+    var UnixTimeToDate = dateObj.getFullYear() + '/' + (dateObj.getMonth() + 1) + '/' + dateObj.getDate() + ' ' + dateObj.getHours() + ':' + dateObj.getMinutes() + ':' + dateObj.getSeconds();
+    document.unix2beijing.result.value = UnixTimeToDate;
+}
+function human2unix() {
+    var humanDate = new Date(Date.UTC(document.beijing2unix.year.value, (stripLeadingZeroes(document.beijing2unix.month.value)-1), stripLeadingZeroes(document.beijing2unix.day.value), stripLeadingZeroes(document.beijing2unix.hour.value), stripLeadingZeroes(document.beijing2unix.minute.value), stripLeadingZeroes(document.beijing2unix.second.value)));
+    document.beijing2unix.result.value = (humanDate.getTime()/1000 - 8*60*60);
+}
+function stripLeadingZeroes(input) {
+    if((input.length > 1) && (input.substr(0,1) == "0")) {
+        return input.substr(1);
+    } else {
+        return input;
+    }
+}
+function currentTime() {
+    var timeNow = new Date();
+    document.getElementById("currentunixtime").innerHTML = Math.round(timeNow.getTime()/1000);
+    document.getElementById("currenttime").innerHTML = new Date().getFullYear()  + "-" + (new Date().getMonth()+1) + "-" + new Date().getDate()+" " + new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds();
+    if (currentTimeActive) {
+        unixTimer = setTimeout("currentTime()", 1000);
+    }
+}
+function startTimer() {
+    currentTimeActive = 1;
+    currentTime();
+}
+function stopTimer() {
+    currentTimeActive = 0;
+    clearTimeout(unixTimer);
+}
+function encodeText() {
+    var rawText  = $("#rawText")[0].value;
+    var encodeText = encodeURIComponent(rawText);
+    $("#encodeText")[0].value = encodeText;
+}
+function decodeText() {
+    var rawText  = $("#rawText")[0].value;
+    var decodeText = decodeURIComponent(rawText);
+    $("#decodeText")[0].value = decodeText;
+}
