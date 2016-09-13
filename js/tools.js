e65cbad42b3039c3500f34a126f6348053c101d2
yifei.xu <yifei.xu@jingle.cn>
2016-09-13 10:05:37 +0800

    [add] more timezone

--- js/tools.js
+++ js/tools.js
@@ -6,8 +6,22 @@ var unixTimer = 0;
 function unix2human() {
     var dateObj = new Date(document.unix2beijing.timestamp.value * 1000);
     var UnixTimeToDate = dateObj.getFullYear() + '/' + formatDate(dateObj.getMonth() + 1) + '/' + formatDate(dateObj.getDate()) + ' ' + formatDate(dateObj.getHours()) + ':' + formatDate(dateObj.getMinutes()) + ':' + formatDate(dateObj.getSeconds());
-    document.unix2beijing.result.value = UnixTimeToDate;
+    document.unix2beijing.resultBJ.value = UnixTimeToDate;
+    //北美时间
+    var d = new Date(document.unix2beijing.timestamp.value * 1000);
+    var c = d.getTime() + d.getTimezoneOffset()*60000;
+    dateObj = new Date(c+3600000*(-4));
+    var UnixTimeToDateBM = dateObj.getFullYear() + '/' + formatDate(dateObj.getMonth() + 1) + '/' + formatDate(dateObj.getDate()) + ' ' + formatDate(dateObj.getHours()) + ':' + formatDate(dateObj.getMinutes()) + ':' + formatDate(dateObj.getSeconds());
+    document.unix2beijing.resultAC.value = UnixTimeToDateBM;
+
+    //泰国时间
+    d = new Date(document.unix2beijing.timestamp.value * 1000);
+    c = d.getTime() + d.getTimezoneOffset()*60000;
+    dateObj = new Date(c+3600000*(+7));
+    document.unix2beijing.resultTH.value = dateObj.getFullYear() + '/' + formatDate(dateObj.getMonth() + 1) + '/' + formatDate(dateObj.getDate()) + ' ' + formatDate(dateObj.getHours()) + ':' + formatDate(dateObj.getMinutes()) + ':' + formatDate(dateObj.getSeconds());
 }
+
+
 function human2unix() {
     var humanDate = new Date(Date.UTC(document.beijing2unix.year.value, (stripLeadingZeroes(document.beijing2unix.month.value) - 1), stripLeadingZeroes(document.beijing2unix.day.value), stripLeadingZeroes(document.beijing2unix.hour.value), stripLeadingZeroes(document.beijing2unix.minute.value), stripLeadingZeroes(document.beijing2unix.second.value)));
     document.beijing2unix.result.value = (humanDate.getTime() / 1000 - 8 * 60 * 60);
